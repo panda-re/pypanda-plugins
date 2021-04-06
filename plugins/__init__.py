13de5db78972ba32241733ef70c898dde1343dcb
Andrew Fasano <fasano@mit.edu>
2021-04-06 11:48:58 -0400

    Add test for Faker + shebang strings

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,9 +2,8 @@
 Note file names should NOT contain underscores, let's keep these in (upper)
 CamelCase going forward (e.g., ModeFilter) so they match the class names.
 """
-from .ModeFilter import ModeFilter
-
-from .FileHook import FileHook
 from .FileFaker import FakeFile
+from .FileHook import FileHook
 from .IoctlFaker import IoctlFaker
+from .ModeFilter import ModeFilter
 from .ProcWriteCapture import ProcWriteCapture
