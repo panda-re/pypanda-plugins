72f797cde2261031f3134ce807f692d28915faf1
Andrew Fasano <fasano@mit.edu>
2021-12-07 18:04:07 -0500

    Fix setup, bump pandare dependency

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-from setuptools.command.develop import develop
+import setuptools
 
-setup(name='pandarepyplugins',
-      version='0.0.1',
-      description='A collection of reusable PyPlugins for PANDA.re',
-      author='Andrew Fasano',
-      author_email='fasano@mit.edu',
-      url='https://github.com/panda-re/pypanda-plugins',
-      packages=find_packages(),
-      #install_requires=[ 'pandare>=0.1.1.3' ],
-      cmdclass={'install': install, 'develop': develop},
-     )
+if __name__ == "__main__":
+    setuptools.setup()
